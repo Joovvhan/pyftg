e29abb6c0d8e1d1f5e327d01dcccbec161278218
Joowhan Song <joovvhan.sonq@gmail.com>
2025-08-31 22:16:51 +0900

    Random Agent Test Code

--- examples/CustomAI.py
+++ examples/CustomAI.py
@@ -1,12 +1,56 @@
 import logging
+import random
 
 from pyftg import (AIInterface, AudioData, CommandCenter, FrameData, GameData,
                    Key, RoundResult, ScreenData)
 
 logger = logging.getLogger(__name__)
 
+POSSIBLE_ACTIONS = [
+    "FORWARD_WALK",
+    "DASH",
+    "BACK_STEP",
+    "CROUCH",
+    "JUMP",
+    "FOR_JUMP",
+    "BACK_JUMP",
+    "STAND_GUARD",
+    "CROUCH_GUARD",
+    "AIR_GUARD",
+    "THROW_A",
+    "THROW_B",
+    "STAND_A",
+    "STAND_B",
+    "CROUCH_A",
+    "CROUCH_B",
+    "AIR_A",
+    "AIR_B",
+    "AIR_DA",
+    "AIR_DB",
+    "STAND_FA",
+    "STAND_FB",
+    "CROUCH_FA",
+    "CROUCH_FB",
+    "AIR_FA",
+    "AIR_FB",
+    "AIR_UA",
+    "AIR_UB",
+    "STAND_D_DF_FA",
+    "STAND_D_DF_FB",
+    "STAND_F_D_DFA",
+    "STAND_F_D_DFB",
+    "STAND_D_DB_BA",
+    "STAND_D_DB_BB",
+    "AIR_D_DF_FA",
+    "AIR_D_DF_FB",
+    "AIR_F_D_DFA",
+    "AIR_F_D_DFB",
+    "AIR_D_DB_BA",
+    "AIR_D_DB_BB",
+    "STAND_D_DF_FC",
+]
 
-class DisplayInfo(AIInterface):
+class CustomAI(AIInterface):
     def __init__(self):
         self.blind_flag = False
         self.width = 96
@@ -42,7 +86,7 @@ class DisplayInfo(AIInterface):
         
     def processing(self):
 
-        # print("This function is called every frame")
+        print("This function is called every frame")
 
         if self.frame_data.empty_flag or self.frame_data.current_frame_number <= 0:
             return
@@ -55,22 +99,24 @@ class DisplayInfo(AIInterface):
         self.cc.skill_cancel()
 
         # calculate the distance
-        try:
-            distance = self.calculate_distance(self.screen_data.display_bytes)
-        except:
-            distance = 0
-        if distance == -1:
-            self.cc.command_call("STAND_A") # default action
-        else:
-            close = 80 * self.width / 960
-            far = 200 * self.width / 960
-            # conduct action according to the distance based on pixel data
-            if distance < close:
-                self.cc.command_call("CROUCH_B")
-            elif distance >= close and distance < far:
-                self.cc.command_call("STAND_FB")
-            else:
-                self.cc.command_call("STAND_D_DF_FA")
+        # try:
+        #     distance = self.calculate_distance(self.screen_data.display_bytes)
+        # except:
+        #     distance = 0
+        # if distance == -1:
+        #     self.cc.command_call("STAND_A") # default action
+        # else:
+        #     close = 80 * self.width / 960
+        #     far = 200 * self.width / 960
+        #     # conduct action according to the distance based on pixel data
+        #     if distance < close:
+        #         self.cc.command_call("CROUCH_B")
+        #     elif distance >= close and distance < far:
+        #         self.cc.command_call("STAND_FB")
+        #     else:
+        #         self.cc.command_call("STAND_D_DF_FA")
+        action = random.choice(POSSIBLE_ACTIONS)
+        self.cc.command_call(action)
                         
     def calculate_distance(self, display_buffer: bytes):
         for y in reversed(range(self.height)):
---
+++ examples/Main_SinglePyAITest.py
@@ -0,0 +1,74 @@
+import subprocess
+import asyncio
+import time
+
+import typer
+from typing_extensions import Annotated, Optional
+
+from KickAI import KickAI
+from DisplayInfo import DisplayInfo
+from CustomAI import CustomAI
+from pyftg.socket.aio.gateway import Gateway
+from pyftg.utils.logging import DEBUG, set_logging
+
+app = typer.Typer(pretty_exceptions_enable=False)
+
+
+async def start_process(host: str, port: int, character: str = "ZEN", game_num: int = 1):
+    gateway = Gateway(host, port)
+    
+    agent1 = KickAI()
+    gateway.register_ai("KickAI", agent1)
+
+    agent2 = DisplayInfo()
+    gateway.register_ai("DisplayInfo", agent2)
+
+    agent3 = CustomAI()
+    gateway.register_ai("CustomAI", agent3)
+
+    await gateway.run_game([character, character], ["CustomAI", "DisplayInfo"], game_num)
+
+
+@app.command()
+def main(
+        host: Annotated[Optional[str], typer.Option(help="Host used by DareFightingICE")] = "127.0.0.1",
+        port: Annotated[Optional[int], typer.Option(help="Port used by DareFightingICE")] = 31415):
+    asyncio.run(start_process(host, port))
+
+
+if __name__ == '__main__':
+    set_logging(log_level=DEBUG)
+
+    # https://github.com/TeamFightingICE/FightingICE/blob/master/src/core/Game.java
+    server_cmd = [
+        "java",
+        "-XstartOnFirstThread",
+        "-cp",
+        "FightingICE.jar:./lib/*:./lib/lwjgl/*:./lib/lwjgl/natives/macos/arm64/*:./lib/grpc/*",
+        "Main",
+        "--limithp", "400", "400",
+        "--grey-bg",
+        "--pyftg-mode",
+        "--input-sync"
+    ]
+    server_proc = subprocess.Popen(server_cmd, cwd="../../DareFightingICE-7.0")
+
+    # server_cmd = """
+    # java -XstartOnFirstThread \
+    # -cp "FightingICE.jar:./lib/*:./lib/lwjgl/*:./lib/lwjgl/natives/macos/arm64/*:./lib/grpc/*" \
+    # Main --limithp 400 400 --grey-bg --pyftg-mode
+    # """
+
+    # # 별도 zsh에서 실행
+    # server_proc = subprocess.Popen(
+    #     ["zsh", "-c", server_cmd],
+    #     cwd="../../DareFightingICE-7.0"
+    # )
+
+    time.sleep(1)
+
+    try:
+        app()
+    finally:
+        server_proc.terminate()
+        server_proc.wait()
